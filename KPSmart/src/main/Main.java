c3bbfd93a3b89e51b7b49be17e332549bdba3996
Shelley O'Neill <shelley.oneill89@gmail.com>
2016-05-01 22:13:52 +1200

    Set up initial project and Graph

---
+++ KPSmart/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.List;
+
+public class Main {
+
+	private List<Location> locations;
+
+	public List<Location> getLocations() {
+		return locations;
+	}
+
+	public void setLocations(List<Location> locations) {
+		this.locations = locations;
+	}
+}
