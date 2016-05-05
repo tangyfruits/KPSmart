771d511622212c2267ba61b721db6e67c785f9b9
Donald Tang <tangyfruits@gmail.com>
2016-05-05 20:50:43 +1200

    Deleted TestObject, replaced String Day with Enum Day. - Donald

--- KPSmart/src/main/Route.java
+++ KPSmart/src/main/Route.java
@@ -12,77 +12,81 @@ public class Route {
 	private int maxVolume;
 	private int duration;
 	private int frquency;
-	private String day;
+	private Day day;
 	private CustomerPrice price;
-	
+
+	private enum Day {
+		Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+	}
+
 	public Location getOrigin() {
 		return origin;
 	}
-	
+
 	public Location getDestination() {
 		return destination;
 	}
-	
+
 	public String getCompany() {
 		return company;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public double getWeightCost() {
 		return weightCost;
 	}
-	
+
 	public void setWeightCost(double weightCost) {
 		this.weightCost = weightCost;
 	}
-	
+
 	public double getVolumeCost() {
 		return volumeCost;
 	}
-	
+
 	public void setVolumeCost(double volumeCost) {
 		this.volumeCost = volumeCost;
 	}
-	
+
 	public int getMaxWeight() {
 		return maxWeight;
 	}
-	
+
 	public void setMaxWeight(int maxWeight) {
 		this.maxWeight = maxWeight;
 	}
-	
+
 	public int getMaxVolume() {
 		return maxVolume;
 	}
-	
+
 	public void setMaxVolume(int maxVolume) {
 		this.maxVolume = maxVolume;
 	}
-	
+
 	public int getDuration() {
 		return duration;
 	}
-	
+
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
-	
+
 	public int getFrquency() {
 		return frquency;
 	}
-	
+
 	public void setFrquency(int frquency) {
 		this.frquency = frquency;
 	}
-	
-	public String getDay() {
+
+	public Day getDay() {
 		return day;
 	}
-	
+
 	public void setDay(String day) {
 		this.day = day;
 	}
@@ -101,5 +105,5 @@ public class Route {
 
 	public void setPriority(String priority) {
 		this.priority = priority;
-	}	
+	}
 }
