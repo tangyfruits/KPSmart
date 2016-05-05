fbb748750fa0751d8b56c6276cccef2e93126149
Donald Tang <tangyfruits@gmail.com>
2016-05-05 21:16:00 +1200

    Added logic to find direct routes. -Donald

--- KPSmart/src/main/Location.java
+++ KPSmart/src/main/Location.java
@@ -7,11 +7,29 @@ public class Location {
 	private String name;
 	private List<Route> routes;
 	private List<CustomerPrice> prices;
+	private List<Route> directRoutes;
+	private double lowestPrice = 999;
 
 	public Location(String name, List<Route> routes, List<CustomerPrice> prices) {
 		this.name = name;
 		this.routes = routes;
 		this.prices = prices;
+	}
+
+	public Route getBestRoute(Location origin, Location destination) {
+		for (Route r : routes) {
+			if (r.getDestination() == destination) {
+				directRoutes.add(r);
+			}
+		}
+		// for (Route r : directRoutes){
+		// if (r.getPrice() < lowestPrice){
+		//
+		// }
+		// }
+
+		return null;
+
 	}
 
 	public String getName() {
