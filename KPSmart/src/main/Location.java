691f73eed74b7b27b4ad510f46fa938c676df18d
Peter Scriven <justabitdope+git@gmail.com>
2016-05-26 20:53:43 +1200

    Location code reformat

--- KPSmart/src/main/Location.java
+++ KPSmart/src/main/Location.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Location {
-
+	// VARIABLES
 	private String name;
 	private String priority;
 	private Location fromLocation;
@@ -12,75 +12,68 @@ public class Location {
 	private List<CustomerPrice> prices = new ArrayList<CustomerPrice>();;
 	private double costSoFar;
 	private boolean visited;
-
-	public String getPriority() {
-		return priority;
-	}
-
-	public void setPriority(String priority) {
-		this.priority = priority;
-	}
-
-	public double getCostSoFar() {
-		return costSoFar;
-	}
-
-	public boolean isVisited() {
-		return visited;
-	}
-
-	public void setVisited(boolean visited) {
-		this.visited = visited;
-	}
-
+	
+	// CONSTRUCTOR
 	public Location(String name) {
 		this.name = name;
 	}
-
+	
+	// METHODS
+	// Getters
+	public String getPriority() {
+		return priority;
+	}
+	public double getCostSoFar() {
+		return costSoFar;
+	}
+	public boolean isVisited() {
+		return visited;
+	}
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public List<Route> getRoutes() {
 		return routes;
 	}
-
-	public void setRoutes(List<Route> routes) {
-		this.routes = routes;
-	}
-
-	public void addRoute(Route route) {
-		this.routes.add(route);
-	}
-
 	public List<CustomerPrice> getPrices() {
 		return prices;
 	}
 
+	// Setters (Adders)
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setRoutes(List<Route> routes) {
+		this.routes = routes;
+	}
+	public void addRoute(Route route) {
+		this.routes.add(route);
+	}
 	public void setPrices(List<CustomerPrice> prices) {
 		this.prices = prices;
 	}
-
 	public void addPrice(CustomerPrice price) {
 		this.prices.add(price);
 	}
-
-	public void setCostSoFar(double costSoFar) {
-		this.costSoFar = costSoFar;
-	}
-
-	public void setFrom(Location l) {
-		this.fromLocation = l;
-	}
-
+	
+	// Others
 	public Location getFrom() {
 		return fromLocation;
 	}
-
+	
+	public void setFrom(Location l) {
+		this.fromLocation = l;
+	}
+	public void setCostSoFar(double costSoFar) {
+		this.costSoFar = costSoFar;
+	}
+	
 	public String toString() {
 		return ("Loc name: " + getName());
 	}
