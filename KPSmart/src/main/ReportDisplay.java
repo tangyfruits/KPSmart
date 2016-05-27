6481c0e01be5d9729a7d04f6a58bdee8116d4be8
Pri Bhula <pridotbhula@gmail.com>
2016-05-28 11:35:42 +1200

    New report display class. damn

---
+++ KPSmart/src/main/ReportDisplay.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.util.ArrayList;
+
+import event.Event;
+
+public class ReportDisplay {
+	public void addTotalRev(){
+		
+	}
+	
+	public void addTotalExp(){
+		
+	}
+	
+	public void addTotalEvents(){
+		
+	}
+	
+	public String getTotalRev(){
+		return null;
+	}
+	
+	public String getTotalExp(){
+		return null;
+	}
+	
+	public ArrayList <Event> getTotalEvents(){
+		ArrayList<Event> events = new ArrayList<Event>();
+		
+		return events;
+	}
+}
