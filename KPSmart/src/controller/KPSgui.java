4593dad2a935cd6cdbde22e300311c7a5955ea80
Shelley O'Neill <shelley.oneill89@gmail.com>
2016-06-12 21:24:06 +1200

    Set up initial load of routes and submit on discontinue form

--- KPSmart/src/controller/KPSgui.java
+++ KPSmart/src/controller/KPSgui.java
@@ -20,9 +20,26 @@ public class KPSgui extends Application {
         main.logCustomerPriceUpdate("Wellington", "Auckland", "Air", 12, 12, false);
         main.logTransportCostUpdate("Wellington", "Auckland", "UPS", "Air", 4, 4, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
         main.logCustomerPriceUpdate("Wellington", "Auckland", "Standard", 2, 5, false);
-        main.logTransportCostUpdate("Wellington", "Auckland", "UPS", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
-		
-		FXMLLoader login = new FXMLLoader(getClass().getResource("/views/login.fxml"));
+        main.logTransportCostUpdate("Wellington", "Auckland", "A", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "B", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "C", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "D", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "E", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "F", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "G", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "H", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+        main.logTransportCostUpdate("Wellington", "Auckland", "J", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "K", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "L", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "M", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "N", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "O", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "P", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "Q", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "R", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+//        main.logTransportCostUpdate("Wellington", "Auckland", "S", "Standard",2 , 1, 15, 15, 12, 24, DayOfWeek.THURSDAY, 12, false);
+       
+        FXMLLoader login = new FXMLLoader(getClass().getResource("/views/login.fxml"));
 		login.setController(new LoginController(main));
 		Parent UI = login.load();
 						
