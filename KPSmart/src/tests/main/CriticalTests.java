f9e735e07c7373d3ed3c5b5b331ce4729e5d9a90
Shelley O'Neill <shelley.oneill89@gmail.com>
2016-06-16 16:47:22 +1200

    Quick fix to tests

--- KPSmart/src/tests/main/CriticalTests.java
+++ KPSmart/src/tests/main/CriticalTests.java
@@ -2,6 +2,8 @@ package tests.main;
 
 import static org.junit.Assert.*;
 
+import java.time.DayOfWeek;
+
 import org.junit.Test;
 
 import main.Main;
@@ -14,9 +16,12 @@ public class CriticalTests {
 
 	public void setUp(){
 		main = new Main();
-		o = "From";
-		d = "To";
-		p = "totes important";
+		main.logCustomerPriceUpdate("Wellington", "Auckland", "Air", 15, 14, false);
+		main.logTransportCostUpdate("Wellington", "Auckland", "UPS", 
+				"Air", 10, 12, 20, 30, 15, 24, DayOfWeek.MONDAY, 12, false);
+		o = "Wellington";
+		d = "Auckland";
+		p = "Air";
 		w = 2.5;
 		v = 1.5;
 		
