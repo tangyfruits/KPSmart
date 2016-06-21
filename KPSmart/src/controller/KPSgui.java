1cf4ebe176432b50913873df39f07b0b4d7b411b
Peter Scriven <justabitdope@gmail.com>
2016-06-21 15:25:11 +1200

    Added command line compile file. Fixed window size bug.

--- KPSmart/src/controller/KPSgui.java
+++ KPSmart/src/controller/KPSgui.java
@@ -11,32 +11,32 @@ import main.LogReader;
 import main.Main;
 
 public class KPSgui extends Application {
-	
+
 	private Main main;
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {		
+	public void start(Stage primaryStage) throws Exception {
 
 		main = new Main();
-		
+
         LogReader reader = new LogReader(main.getLogFile(), main);
     	reader.parseFile();
-		
+
         FXMLLoader login = new FXMLLoader(getClass().getResource("/views/login.fxml"));
 		login.setController(new LoginController(main));
 		Parent UI = login.load();
-						
+
 		primaryStage.setTitle("KPSmart");
-		primaryStage.setMinWidth(720);
-		primaryStage.setMinHeight(620);
+		primaryStage.setMinWidth(700);
+		primaryStage.setMinHeight(600);
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../views/icon.png"))); // must be in /bin/views
-		
+
 		Scene scene = new Scene(UI, 700, 600);
 		primaryStage.setScene(scene);
-		
+
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
